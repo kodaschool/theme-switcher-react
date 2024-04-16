fb4ab999d6016da0c9b30bf0a28e47a075043ce7
Mary <marieshix98@gmail.com>
2024-04-16 17:34:13 +0300

    add method to toggle react icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 import { useTheme } from "../context/useThemeContext";
+import { MdLightMode } from "react-icons/md";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
   const handleToggle = () => {
     toggleTheme();
   };
   return (
-    <nav className="flex h-20 items-center justify-between">
-      <h1 className="italics">Buy Sasa</h1>
-      <div onClick={handleToggle}>Theme switcher</div>
+    <nav className="flex h-20  items-center justify-between">
+      <h1 className="italics">BuySasa</h1>
+      <div
+        onClick={handleToggle}
+        className="w-[40px] h-[40px] flex items-center justify-center rounded-md border-2 border-darkmode dark:border-white"
+      >
+        {theme ? <BsFillMoonStarsFill size={20} /> : <MdLightMode size={20} />}
+      </div>
     </nav>
   );
 };
